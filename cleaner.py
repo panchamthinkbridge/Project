efe0fd99c944043b96a4b3eb1ddbb46238fd455d
panchamthinkbridge <pancham@thinkbridge.in>
2021-01-15 16:11:21 +0530

    First Commit

---
+++ cleaner.py
@@ -0,0 +1,31 @@
+import os
+import glob
+import argparse
+
+def text_cleaner(fileName):
+    with open(fileName, encoding='utf8', errors='ignore') as reader:
+        text = reader.read()
+        text = " ".join(text.split())
+        reader.close()
+
+    return text
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str)
+    args = parser.parse_args()
+
+    folderName = 'Clean-JD'
+    os.makedirs(folderName)
+    fileGLOB = glob.glob(f'{args.path}\*.txt') #get path of all text files
+
+    for file in fileGLOB:
+        text = text_cleaner(file)
+        fname = file.split('\\')[1]
+        print(fname)
+        with open(f'{folderName}\clean-{fname}', 'w',encoding='utf8', errors='ignore') as writer:
+            writer.write(text)
+        writer.close()
+        
+
+    print('DONE')
